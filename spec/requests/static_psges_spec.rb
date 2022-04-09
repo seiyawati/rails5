37d21f6aab6de30f077a769b18ea2aff15056daf
seiyawati <kid960805@gmail.com>
2022-04-09 21:23:01 +0900

    add: chapter4

--- spec/requests/static_psges_spec.rb
+++ spec/requests/static_psges_spec.rb
@@ -4,36 +4,36 @@ RSpec.describe 'StaticPages', type: :request do
   let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
   describe '#home' do
-    it '正常にレスポンスを返すこと' do
+    it 'should get home' do
       get static_pages_home_path
       expect(response).to have_http_status :ok
     end
 
-    it 'Home | Ruby on Rails Tutorial Sample Appが含まれること' do
+    it 'should include Ruby on Rails Tutorial Sample App' do
       get static_pages_home_path
-      expect(response.body).to include "Home | #{base_title}"
+      expect(response.body).to include "#{base_title}"
     end
   end
 
   describe '#help' do
-    it '正常にレスポンスを返すこと' do
+    it 'should get help' do
       get static_pages_help_path
       expect(response).to have_http_status :ok
     end
 
-    it 'Help | Ruby on Rails Tutorial Sample Appが含まれること' do
+    it 'should include Help | Ruby on Rails Tutorial Sample App' do
       get static_pages_help_path
       expect(response.body).to include "Help | #{base_title}"
     end
   end
 
   describe '#about' do
-    it '正常にレスポンスを返すこと' do
+    it 'should get about' do
       get static_pages_about_path
       expect(response).to have_http_status :ok
     end
 
-    it 'About | Ruby on Rails Tutorial Sample Appが含まれること' do
+    it 'should include About | Ruby on Rails Tutorial Sample App' do
       get static_pages_about_path
       expect(response.body).to include "About | #{base_title}"
     end
