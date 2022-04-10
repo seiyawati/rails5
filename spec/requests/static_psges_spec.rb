f902276d19051ff4bbb4aa63a7a38cce75722ac8
seiyawati <kid960805@gmail.com>
2022-04-10 13:32:46 +0900

    add: chapter5

--- spec/requests/static_psges_spec.rb
+++ spec/requests/static_psges_spec.rb
@@ -5,37 +5,49 @@ RSpec.describe 'StaticPages', type: :request do
 
   describe '#home' do
     it 'should get home' do
-      get static_pages_home_path
+      get root_path
       expect(response).to have_http_status :ok
     end
 
     it 'should include Ruby on Rails Tutorial Sample App' do
-      get static_pages_home_path
+      get root_path
       expect(response.body).to include "#{base_title}"
     end
   end
 
   describe '#help' do
     it 'should get help' do
-      get static_pages_help_path
+      get help_path
       expect(response).to have_http_status :ok
     end
 
     it 'should include Help | Ruby on Rails Tutorial Sample App' do
-      get static_pages_help_path
+      get help_path
       expect(response.body).to include "Help | #{base_title}"
     end
   end
 
   describe '#about' do
     it 'should get about' do
-      get static_pages_about_path
+      get about_path
       expect(response).to have_http_status :ok
     end
 
     it 'should include About | Ruby on Rails Tutorial Sample App' do
-      get static_pages_about_path
+      get about_path
       expect(response.body).to include "About | #{base_title}"
+    end
+  end
+
+  describe '#contact' do
+    it 'should get contact' do
+      get contact_path
+      expect(response).to have_http_status :ok
+    end
+
+    it 'should include Contact | Ruby on Rails Tutorial Sample App' do
+      get contact_path
+      expect(response.body).to include "Contact | #{base_title}"
     end
   end
 end
